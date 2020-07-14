a3cdf249d9cd143f3c8db9599ec9ffa84d4b4c11
Hishaam <h.ali1090@outlook.com>
2020-07-14 14:07:17 +0100

    Connect to MongoDB and Route Files

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewURLParser: true,
+    });
+    // Display a confirmation on mongodb connection
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    // Display an error message if failed to connect
+    console.error(err.message);
+    // Exit the process on a failure to connect
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
