e3244048aecbcfbe60a3e29af2bde4c9c6f954a3
Hishaam <h.ali1090@outlook.com>
2020-07-18 12:34:01 +0100

    Set up login and register routes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Landing = () => {
   return (
@@ -11,12 +12,12 @@ export const Landing = () => {
             own quizzes!
           </p>
           <div className='buttons'>
-            <a href='register.html' className='btn btn-primary'>
+            <Link to='/register' className='btn btn-primary'>
               Join!
-            </a>
-            <a href='login.html' className='btn btn-light'>
+            </Link>
+            <Link to='/login' className='btn btn-light'>
               Login!
-            </a>
+            </Link>
           </div>
         </div>
       </div>
