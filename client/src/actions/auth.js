5c6e2111029d95afc0b4f830e0135545862bb2cc
Hishaam <h.ali1090@outlook.com>
2020-08-04 13:48:05 +0100

    Adding authentication

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,31 @@
 import axios from 'axios';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from './types';
 import { setAlert } from './alert';
+import setAuthToken from '../utils/setAuthToken';
+
+// Load a user
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get('api/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
 
 // Registers a user
 export const register = ({ name, email, password }) => async (dispatch) => {
