d3b48c2d1af228fa7cae375790c406309d91e4ea
Hishaam <h.ali1090@outlook.com>
2021-03-10 16:35:34 +0000

    adding feedback routes

---
+++ client/src/components/feedback/feedback.js
@@ -0,0 +1,16 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
+import { connect } from 'react-redux';
+import { sendFeedback } from '../../actions/feedback';
+
+<Fragment>
+
+<Link to='/results' className='btn btn-primary my-1'>
+            Click to view results.
+          </Link>
+          <Link to='/profile' className='btn btn-primary my-1'>
+            Click to go back to your profile.
+          </Link>
+
+</Fragment>
